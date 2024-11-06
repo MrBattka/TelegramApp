8e18d0a76f72469fe02918c26e8e1213b2991a8c
MrBaTTka <ilyha_96@inbox.ru>
2024-11-06 17:25:15 +0300

    add date

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ const App = () => {
     getForecast(setWeather, location.latitude, location.longitude)
   }, [])
 
-  console.log(weather);
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
   
 
   return (
     <div className="App">
+      <div className='date'>{`${day}.${month}.${year}`}</div>
       <h1>{weather}<span>℃</span></h1>
     </div>
   );
